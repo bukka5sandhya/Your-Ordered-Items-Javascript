8fef7d50849f575aed00301d374c1241ebd5a8cb
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2024-03-01 17:27:51 +0530

    solution

---
+++ index.js
@@ -0,0 +1,45 @@
+let itemList = [{
+    itemName:"Veg Biryani",
+    uniqueNo:1,
+   },
+   {
+    itemName:"Chicken 65",
+    uniqueNo:2,
+   },
+   {
+    itemName:"Paratha",
+    uniqueNo:3,
+   },
+];
+
+let orderedListContainerEle = document.getElementById('orderedListContainer');
+
+function onDeleteItem(itemId){
+    let deleteEle = document.getElementById(itemId);
+    orderedListContainerEle.appendChild(deleteEle);
+}
+function createAndAppend(item){
+    let itemId = "item"+item.uniqueNo;
+    let buttonId = "button"+item.uniqueNo;
+
+    let deleteItemElement = document.createElement("li");
+    deleteItemElement.id = itemId;
+    deleteItemElement.classList.add("order-item");
+    deleteItemElement.textContent =  item.itemName;
+    orderedListContainerEle.appendChild(deleteItemElement);
+
+    let buttonElement = document.createElement("button");
+    buttonElement.classList.add("btn","btn-danger","ml-3");
+    buttonElement.id=buttonId;
+    buttonElement.textContent = "Cancel";
+
+    buttonElement.onclick = function(){
+        onDeleteItem(itemId);
+    };
+    deleteItemElement.appendChild(buttonElement);
+
+
+}
+for(let eachItem of itemList){
+    createAndAppend(eachItem);
+}
